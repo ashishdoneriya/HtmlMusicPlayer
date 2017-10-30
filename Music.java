bd448fd7aa5a6a8b63bf54c9c6e1b781bde84936
Ashish Doneriya <ashishdoneriya@gmail.com>
2017-10-30 19:23:35 +0530

    Recreated using vuejs

--- Music.java
+++ Music.java
@@ -11,9 +11,14 @@ public class Music {
 	private String fileName;
 
 	private List<Music> files;
+	
+	public int level = 0;
+	
+	public static int id = 0;
 
-	public Music(File file) {
+	public Music(File file, int level) {
 		fileName = file.getName();
+		this.level = level;
 		if (file.isDirectory()) {
 			isDir = true;
 			files = new ArrayList<Music>();
@@ -62,16 +67,16 @@ public class Music {
 		files.add(file);
 	}
 
-	public static Music getMusic(File dir) {
+	public static Music getMusic(File dir, int level) {
 		if (!dir.isDirectory()) {
-			return new Music(dir);
+			return new Music(dir, level);
 		}
-		Music music = new Music(dir);
+		Music music = new Music(dir, level);
 		for (File file : dir.listFiles()) {
 			if (file.isDirectory()) {
-				music.addFile(getMusic(file));
+				music.addFile(getMusic(file, level + 1));
 			} else {
-				music.addFile(new Music(file));
+				music.addFile(new Music(file, level + 1));
 			}
 		}
 		return music;
@@ -79,14 +84,16 @@ public class Music {
 	
 	public static void main(String[] args) {
 		File file = new File("/d/My Documents/My Music");
-		System.out.println(getJson(getMusic(file)));
+		System.out.println(getJson(getMusic(file, 0)));
 	}
 	
 	public static String getJson(Music music) {
 		StringBuffer json = new StringBuffer();
 		json.append("{");
-		json.append("\"isDir\":\"").append(music.isDir).append("\",");
+		json.append("\"id\":").append(id++).append(",");
+		json.append("\"isDir\":").append(music.isDir).append(",");
 		json.append("\"fileName\":\"").append(music.fileName).append("\",");
+		json.append("\"level\":\"").append(music.level).append("\",");
 		if (music.isDir) {
 			json.append("\"files\":[");
 			for (Music m : music.files) {
@@ -101,3 +108,4 @@ public class Music {
 		return json.toString();
 	}
 }
+
